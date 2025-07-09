911b7b4698abe98791c7f6f0fc106caf7fb5ba73
Ognjen Radovanovic <ognjen34car@gmail.com>
2025-07-09 19:09:38 +0200

    initial commit

---
+++ Finiti/Finiti.DATA/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finiti.DATA
+{
+    
+        public class DatabaseContext : DbContext
+        {
+            
+
+            public DatabaseContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+               
+
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseLazyLoadingProxies();
+
+
+            }
+        }  
+}
---
+++ Finiti/Finiti.WEB/Program.cs
@@ -0,0 +1,38 @@
+using Finiti.DATA;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    //connection string je ovde privremeno zbog lakseg developmenta 
+    options.UseNpgsql("Server=localhost;Port=5432;Database=finiti;User Id=finiti;Password=admin;");
+
+
+}, ServiceLifetime.Scoped);
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
