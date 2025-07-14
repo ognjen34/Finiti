92c851a134f3e7e2185c7a84a80e72d7683d0dc7
Ognjen Radovanovic <ognjen34car@gmail.com>
2025-07-15 01:08:38 +0200

    changes to the script

--- start.ps1
+++ start.ps1
@@ -3,10 +3,12 @@
 Write-Host "🔄 Starting Docker containers..."
 docker-compose -f "Finiti\docker-compose.yaml" up -d
 
+Start-Sleep -Seconds 10
+
 Write-Host "🚀 Running .NET backend..."
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd Finiti/Finiti.WEB; dotnet run"
 
 Write-Host "🌐 Running Angular frontend..."
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd client; ng serve --open"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd client; npm install; ng serve --open"
 
 Write-Host "✅ All services started."
